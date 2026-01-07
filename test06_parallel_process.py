d2694a86921b9796e98c2fc836e379ef17aee809
Eric Gustafson <eric.gustafson@explorationinstitute.com>
2026-01-07 20:15:02 +0000

    finalizing, hopefully, parameters

--- test06_parallel_process.py
+++ test06_parallel_process.py
@@ -215,10 +215,17 @@ def main():
 
     # Define parameter grid
     cameras = [0, 1]
+    # cameras = [0]
+
     tiles = range(6)  # [0, 1, 2, 3, 4, 5]
+    # tiles = [0]
+
     # methods = ['average', 'max']
     methods = ['max']
-    iqr_factors = [2.0, 2.5, 3.0]
+
+    # iqr_factors = [2.0, 2.5, 3.0]
+    iqr_factors = [2.0]
+
     # factors = [16, 32]
     factors = [16]
 
